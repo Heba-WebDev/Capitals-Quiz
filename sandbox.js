b61fd8c665ec9339b2a5a2c717dc1a9d0116a988
Heba-Webdev23 <jtvonline@yahoo.com>
2021-12-12 03:49:13 +0300

    the quiz is working

--- sandbox.js
+++ sandbox.js
@@ -7,13 +7,17 @@ let score = document.querySelector('.score');
 
 let result = document.querySelector('.result');
 
-let scoreCounter = 0;
-
 let questionsCounter = 0;
 
 let CorrectAnswer = '';
 
 let group1 = document.querySelectorAll('.group1');
+let group2 = document.querySelectorAll('.group2');
+let group3 = document.querySelectorAll('.group3');
+let group4 = document.querySelectorAll('.group4');
+let group5 = document.querySelectorAll('.group5');
+
+
 
 let questionOne = document.querySelector('.question');
 let questionTwo = document.querySelector('.questionTwo');
@@ -181,6 +185,7 @@ function showResult() {
  
   questionFive.style.display = 'none';
   result.style.display = 'block';
+  score.innerHTML = `${questionsCounter }`
   
 }
 
@@ -197,9 +202,42 @@ function startoverQ() {
   result.style.display = 'none';
   starter.style.display = '';
   answersArr.length = 0;
+  console.log(questionsCounter);
   group1.forEach((x) => {
     x.style.pointerEvents = '';
+    x.classList.add('answer');
+    x.classList.remove('correct');
+    x.classList.remove('wrong');
   })
+
+  group2.forEach((x) => {
+    x.style.pointerEvents = '';
+    x.classList.add('answer');
+    x.classList.remove('correct');
+    x.classList.remove('wrong');
+  })
+
+  group3.forEach((x) => {
+    x.style.pointerEvents = '';
+    x.classList.add('answer');
+    x.classList.remove('correct');
+    x.classList.remove('wrong');
+  })
+
+  group4.forEach((x) => {
+    x.style.pointerEvents = '';
+    x.classList.add('answer');
+    x.classList.remove('correct');
+    x.classList.remove('wrong');
+  })
+
+  group5.forEach((x) => {
+    x.style.pointerEvents = '';
+    x.classList.add('answer');
+    x.classList.remove('correct');
+    x.classList.remove('wrong');
+  })
+  questionsCounter = 0;
 }
 
 
@@ -208,9 +246,12 @@ let firstAnswer = document.querySelector('#one');
 firstAnswer.addEventListener('click', isitrigh);
 
 function isitrigh() {
-  if(firstAnswer.innerHTML == answersArr[0]) {
+
+  if(answersArr.includes(firstAnswer.innerHTML)) {
     firstAnswer.classList.remove('answer');
     firstAnswer.classList.add('correct');
+    questionsCounter++;
+    console.log(answersArr)
     group1.forEach((x) => {
       x.style.pointerEvents = 'none';
     })
@@ -222,3 +263,463 @@ function isitrigh() {
     })
   }
 }
+
+
+let secondAnswer = document.querySelector('#two');
+
+secondAnswer.addEventListener('click', rightTwo);
+
+function rightTwo() {
+  
+  if(answersArr.includes(secondAnswer.innerHTML)) {
+    secondAnswer.classList.remove('answer');
+    secondAnswer.classList.add('correct');
+    questionsCounter++;
+    group1.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    secondAnswer.classList.remove('answer');
+    secondAnswer.classList.add('wrong');
+    group1.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+let thirdAnswer = document.querySelector('#three');
+
+thirdAnswer.addEventListener('click', rightThree);
+
+function rightThree() {
+  
+  if(answersArr.includes(thirdAnswer.innerHTML)) {
+    thirdAnswer.classList.remove('answer');
+    thirdAnswer.classList.add('correct');
+    questionsCounter++;
+    console.log(answersArr)
+    group1.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    thirdAnswer.classList.remove('answer');
+    thirdAnswer.classList.add('wrong');
+    group1.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+let fourthAnswer = document.querySelector('#four');
+
+fourthAnswer.addEventListener('click', rightFour);
+
+function rightFour() {
+  
+  if(answersArr.includes(fourthAnswer.innerHTML)) {
+    fourthAnswer.classList.remove('answer');
+    fourthAnswer.classList.add('correct');
+    questionsCounter++;
+    group1.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    fourthAnswer.classList.remove('answer');
+    fourthAnswer.classList.add('wrong');
+    group1.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+let fiveAnswer = document.querySelector('#five');
+
+fiveAnswer.addEventListener('click', fiveCorrect);
+
+function fiveCorrect() {
+  if(answersArr.includes(fiveAnswer.innerHTML)) {
+    fiveAnswer.classList.remove('answer');
+    fiveAnswer.classList.add('correct');
+    questionsCounter++;
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    fiveAnswer.classList.remove('answer');
+    fiveAnswer.classList.add('wrong');
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+let sixAnswer = document.querySelector('#six');
+
+sixAnswer.addEventListener('click', sixCorrect);
+
+function sixCorrect() {
+  if(answersArr.includes(sixAnswer.innerHTML)) {
+    sixAnswer.classList.remove('answer');
+    sixAnswer.classList.add('correct');
+    questionsCounter++;
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    sixAnswer.classList.remove('answer');
+    sixAnswer.classList.add('wrong');
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+let seventhAnswer = document.querySelector('#seven');
+
+seventhAnswer.addEventListener('click', rightSeven);
+
+function rightSeven() {
+  
+  if(answersArr.includes(seventhAnswer.innerHTML)) {
+    seventhAnswer.classList.remove('answer');
+    seventhAnswer.classList.add('correct');
+    questionsCounter++;
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    seventhAnswer.classList.remove('answer');
+    seventhAnswer.classList.add('wrong');
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+let eigthAnswer = document.querySelector('#eight');
+
+eigthAnswer.addEventListener('click', rightEight);
+
+function rightEight() {
+  
+  if(answersArr.includes(eigthAnswer.innerHTML)) {
+    eigthAnswer.classList.remove('answer');
+    eigthAnswer.classList.add('correct');
+    questionsCounter++;
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    eigthAnswer.classList.remove('answer');
+    eigthAnswer.classList.add('wrong');
+    group2.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+let nineAnswer = document.querySelector('#nine');
+
+nineAnswer.addEventListener('click', rightNine);
+
+function rightNine() {
+  
+  if(answersArr.includes(nineAnswer.innerHTML)) {
+    nineAnswer.classList.remove('answer');
+    nineAnswer.classList.add('correct');
+    questionsCounter++;
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    nineAnswer.classList.remove('answer');
+    nineAnswer.classList.add('wrong');
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+
+let tenAnswer = document.querySelector('#ten');
+
+tenAnswer.addEventListener('click', rightTen);
+
+function rightTen() {
+  
+  if(answersArr.includes(tenAnswer.innerHTML)) {
+    tenAnswer.classList.remove('answer');
+    tenAnswer.classList.add('correct');
+    questionsCounter++;
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    tenAnswer.classList.remove('answer');
+    tenAnswer.classList.add('wrong');
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+let elevenAnswer = document.querySelector('#eleven');
+
+elevenAnswer.addEventListener('click', rightEleven);
+
+function rightEleven() {
+  
+  if(answersArr.includes(elevenAnswer.innerHTML)) {
+    elevenAnswer.classList.remove('answer');
+    elevenAnswer.classList.add('correct');
+    questionsCounter++;
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    elevenAnswer.classList.remove('answer');
+    elevenAnswer.classList.add('wrong');
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+let twelveAnswer = document.querySelector('#twelve');
+
+twelveAnswer.addEventListener('click', rightTwelve);
+
+function rightTwelve() {
+  
+  if(answersArr.includes(twelveAnswer.innerHTML)) {
+    twelveAnswer.classList.remove('answer');
+    twelveAnswer.classList.add('correct');
+    questionsCounter++;
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    twelveAnswer.classList.remove('answer');
+    twelveAnswer.classList.add('wrong');
+    group3.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+
+ 
+
+let thirteenAnswer = document.querySelector('#thirteen');
+
+thirteenAnswer.addEventListener('click', rightThirteen);
+
+function rightThirteen() {
+  
+  if(answersArr.includes(thirteenAnswer.innerHTML)) {
+    thirteenAnswer.classList.remove('answer');
+    thirteenAnswer.classList.add('correct');
+    questionsCounter++;
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    thirteenAnswer.classList.remove('answer');
+    thirteenAnswer.classList.add('wrong');
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+
+
+let fourteenAnswer = document.querySelector('#fourteen');
+
+fourteenAnswer.addEventListener('click', rightFourteen);
+
+function rightFourteen() {
+  
+  if(answersArr.includes(fourteenAnswer.innerHTML)) {
+    fourteenAnswer.classList.remove('answer');
+    fourteenAnswer.classList.add('correct');
+    questionsCounter++;
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    fourteenAnswer.classList.remove('answer');
+    fourteenAnswer.classList.add('wrong');
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+
+let fifteenAnswer = document.querySelector('#fifteen');
+
+fifteenAnswer.addEventListener('click', rightFifteen);
+
+function rightFifteen() {
+  
+  if(answersArr.includes(fifteenAnswer.innerHTML)) {
+    fifteenAnswer.classList.remove('answer');
+    fifteenAnswer.classList.add('correct');
+    questionsCounter++;
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    fifteenAnswer.classList.remove('answer');
+    fifteenAnswer.classList.add('wrong');
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+
+
+let sixteenAnswer = document.querySelector('#sixteen');
+
+sixteenAnswer.addEventListener('click', rightSixteen);
+
+function rightSixteen() {
+  
+  if(answersArr.includes(sixteenAnswer.innerHTML)) {
+    sixteenAnswer.classList.remove('answer');
+    sixteenAnswer.classList.add('correct');
+    questionsCounter++;
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    sixteenAnswer.classList.remove('answer');
+    sixteenAnswer.classList.add('wrong');
+    group4.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+
+let seventeenAnswer = document.querySelector('#seventeen');
+
+seventeenAnswer.addEventListener('click', rightSeventeen);
+
+function rightSeventeen() {
+  
+  if(answersArr.includes(seventeenAnswer.innerHTML)) {
+    seventeenAnswer.classList.remove('answer');
+    seventeenAnswer.classList.add('correct');
+    questionsCounter++;
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    seventeenAnswer.classList.remove('answer');
+    seventeenAnswer.classList.add('wrong');
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+let eighteenAnswer = document.querySelector('#eighteen');
+
+eighteenAnswer.addEventListener('click', rightEighteen);
+
+function rightEighteen() {
+  
+  if(answersArr.includes(eighteenAnswer.innerHTML)) {
+    eighteenAnswer.classList.remove('answer');
+    eighteenAnswer.classList.add('correct');
+    questionsCounter++;
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    eighteenAnswer.classList.remove('answer');
+    eighteenAnswer.classList.add('wrong');
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+let nineteenAnswer = document.querySelector('#nineteen');
+
+nineteenAnswer.addEventListener('click', rightNineteen);
+
+function rightNineteen() {
+  
+  if(answersArr.includes(nineteenAnswer.innerHTML)) {
+    nineteenAnswer.classList.remove('answer');
+    nineteenAnswer.classList.add('correct');
+    questionsCounter++;
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    nineteenAnswer.classList.remove('answer');
+    nineteenAnswer.classList.add('wrong');
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+let twentyAnswer = document.querySelector('#twenty');
+
+twentyAnswer.addEventListener('click', rightTwenty);
+
+function rightTwenty() {
+  
+  if(answersArr.includes(twentyAnswer.innerHTML)) {
+    twentyAnswer.classList.remove('answer');
+    twentyAnswer.classList.add('correct');
+    questionsCounter++;
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  } else {
+    twentyAnswer.classList.remove('answer');
+    twentyAnswer.classList.add('wrong');
+    group5.forEach((x) => {
+      x.style.pointerEvents = 'none';
+    })
+  }
+}
+
+
+
+
+
